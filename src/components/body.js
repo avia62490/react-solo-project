cef1c4a74460fc15c97a1fa3014de7fe9f0c00d4
Alex Via <alexandervia@gmail.com>
2022-12-14 12:34:54 -0600

    styles email button

--- src/components/body.js
+++ src/components/body.js
@@ -7,15 +7,13 @@ export default function Body() {
             <h3 className="body--job_title">Full Stack Developer</h3>
             <a href="mailto:alexandervia@gmail.com?subject=solo project">
                 <button className="email_button">
-                <img src={email} className="email_icon" alt="email" width="16px"/>
-                <h6>Email</h6>
+                <img src={email} className="email_icon" alt="email" />
+                <h3>Email</h3>
             </button>    
             </a>
-            <div className="body--about">
+            <div className="body--details">
             <h2>About</h2>
                 <h6>I am a full stack developer eager to work on exciting projects in a team environment. I enjoy solving coding problems with creative solutions in order to create finished products that run smoothly and are aesthetically pleasing.</h6>
-            </div>
-            <div className="body--interests">
                 <h2>Interests</h2>
                 <h6>Camping/ hiking. Nature. Yoga. Video games. DnD. Simpsons. Star Wars. Bike riding.</h6>
             </div>
