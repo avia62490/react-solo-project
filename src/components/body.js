1aef7452c2683393c6b902779b7448e60a6b9751
Alex Via <alexandervia@gmail.com>
2022-12-13 20:26:09 -0600

    adds links to email, github, linkedin

--- src/components/body.js
+++ src/components/body.js
@@ -5,12 +5,14 @@ export default function Body() {
         <div className="body">
             <h1 className="body--name">Alex Via</h1>
             <h3 className="body--job_title">Full Stack Developer</h3>
-            <button className="email_button">
+            <a href="mailto:alexandervia@gmail.com?subject=solo project">
+                <button className="email_button">
                 <img src={email} className="email_icon" alt="email" width="16px"/>
                 <h6>Email</h6>
-            </button>
+            </button>    
+            </a>
             <div className="body--about">
-                <h2>About</h2>
+            <h2>About</h2>
                 <h6>I am a full stack developer eager to work on exciting projects in a team environment. I enjoy solving coding problems with creative solutions in order to create finished products that run smoothly and are aesthetically pleasing.</h6>
             </div>
             <div className="body--interests">
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,14 @@ import github_icon from "../images/github_icon.svg"
 import linkedin_icon from "../images/linkedin_icon.svg"
 
 export default function Footer() {
-  return (
-    <div className="footer">
-      <img src={github_icon} className="github_icon" alt="github" />
-      <img src={linkedin_icon} className="linkedin_icon" alt="github" />
-    </div>
-  )
+    return (
+        <div className="footer">
+            <a href="https://github.com/avia62490" target="_blank" rel='noreferrer'>
+                <img src={github_icon} className="github_icon" alt="github" />
+            </a>
+            <a href="https://www.linkedin.com/in/alex-via/" target="_blank" rel='noreferrer'>
+                <img src={linkedin_icon} className="linkedin_icon" alt="github" />
+            </a>
+        </div>
+    );
 }
